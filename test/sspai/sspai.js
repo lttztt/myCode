8dcc43dbc87da4e49707202aa6bd50770a515856
liuzhe <zhethink@gmail.com>
2018-08-18 14:58:02 +0800

    加上顶部progress

--- test/sspai/sspai.js
+++ test/sspai/sspai.js
@@ -12,3 +12,26 @@ $(function(){
     event.stopPropagation()
   })
 })
+// 进度条
+// (function (){
+  var $w = $(window);
+  var $pro = $('.progress-indicator');
+  var wh = $w.height();
+  var bh = $('body').height();
+  console.log('body.height = ' + bh);
+  
+  var sHeight = bh - wh;
+  console.log('sHeight = '+ sHeight);
+  $w.on('scroll',function(){
+    window.requestAnimationFrame(function(){
+      var prec = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
+      console.log($w.scrollTop() / sHeight);
+      
+      updateProgress(prec);
+    })
+  })
+
+  function updateProgress(prec){
+    $pro.css({'width': prec * 100 + '%'})
+  }
+// }());
