146b998c922753ca3365194589f45f6bcdb79cfc
liuzhe <zhethink@gmail.com>
2018-08-18 18:08:25 +0800

    commit

--- test/sspai/sspai.js
+++ test/sspai/sspai.js
@@ -1,3 +1,21 @@
+// 进度条
+(function(){
+  var $w = $(window);
+  var $pro = $('.progress-indicator');
+  var wh = $w.height();
+  var bh = $('body').height();
+  var sHeight = bh - wh;
+  $w.on('scroll',function(){
+    window.requestAnimationFrame(function(){
+      var prec = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
+      updateProgress(prec);
+    })
+  })
+
+  function updateProgress(prec){
+    $pro.css({'width': prec * 100 + '%'})
+  }
+}());
 $(function(){
   $('.search-icon').click(function(event){
     $('.search-bar').addClass('expand');
@@ -11,27 +29,9 @@ $(function(){
   $('.search-bar input').click(function(event){
     event.stopPropagation()
   })
-})
-// 进度条
-// (function (){
-  var $w = $(window);
-  var $pro = $('.progress-indicator');
-  var wh = $w.height();
-  var bh = $('body').height();
-  console.log('body.height = ' + bh);
-  
-  var sHeight = bh - wh;
-  console.log('sHeight = '+ sHeight);
-  $w.on('scroll',function(){
-    window.requestAnimationFrame(function(){
-      var prec = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
-      console.log($w.scrollTop() / sHeight);
-      
-      updateProgress(prec);
-    })
-  })
 
-  function updateProgress(prec){
-    $pro.css({'width': prec * 100 + '%'})
-  }
-// }());
+  $('a').on('click',function(e){
+    e.preventDefault();
+    console.log(e.currentTarget);
+  })
+})
