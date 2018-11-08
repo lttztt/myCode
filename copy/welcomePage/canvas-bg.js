78929186acb0b20904e158d9cd32fd550c98caff
liuzhe <zhethink@gmail.com>
2018-11-08 11:36:45 +0800

    找到canvas的欢迎界面,修改延迟时间

--- copy/welcomePage/canvas-bg.js
+++ copy/welcomePage/canvas-bg.js
@@ -226,7 +226,7 @@ S.UI = (function() {
                 default:
                     S.Shape.switchShape(S.ShapeBuilder.letter(current[0] === cmd ? 'What?' : current));
             }
-        }, 2000, sequence.length, '', showTitles);
+        }, 4000, sequence.length, '', showTitles);
     }
 
 
